50559657ee6c45dd6ce160784f12673e263a86fa
kinakeyoy <erojasr@unicartagena.edu.co>
2018-03-17 22:16:03 -0500

    pantalla de la 1 ala 6  debimente conectorizadas todas

--- PoolCareSystemv3/PCSv3/pcsv3/pcsv3/Views/Page003.xaml.cs
+++ PoolCareSystemv3/PCSv3/pcsv3/pcsv3/Views/Page003.xaml.cs
@@ -7,7 +7,13 @@ namespace pcsv3.Views
         public Page003()
         {
             InitializeComponent();
+            buttonsignup.Clicked += Buttonsignup_Clicked;
             btnforgot.Clicked += Btnforgot_Clicked;
+        }
+
+        private void Buttonsignup_Clicked(object sender, System.EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new Page006());
         }
 
         private void Btnforgot_Clicked(object sender, System.EventArgs e)
