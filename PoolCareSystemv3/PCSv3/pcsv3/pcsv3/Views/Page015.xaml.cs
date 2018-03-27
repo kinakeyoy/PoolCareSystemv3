7c0f2291aca8825172b8770a69e81fe276c47dfc
Enrique Rojas Reyes <erojasr@unicartagena.edu.co>
2018-03-27 14:52:43 -0500

    add pag 15 proved

---
+++ PoolCareSystemv3/PCSv3/pcsv3/pcsv3/Views/Page015.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace pcsv3.Views
+{
+    public partial class Page015 : ContentPage
+    {
+        public Page015()
+        {
+            InitializeComponent();
+        }
+    }
+}
