19723469a247a2aaafedd46bb930f6ca8747fe43
Moncho Oconit <djmonshy_tinoco@hotmail.com>
2018-03-29 18:20:15 -0500

    Page 023 - lista

--- PoolCareSystemv3/PCSv3/pcsv3/pcsv3/App.xaml.cs
+++ PoolCareSystemv3/PCSv3/pcsv3/pcsv3/App.xaml.cs
@@ -54,8 +54,8 @@ namespace pcsv3
             containerRegistry.RegisterForNavigation<Page028>();
             containerRegistry.RegisterForNavigation<Page033>();
             containerRegistry.RegisterForNavigation<Page023>();
-            containerRegistry.RegisterForNavigation<PrismContentPage1>();
-            containerRegistry.RegisterForNavigation<Page023>();
+            
+            
         }
     }
 }
